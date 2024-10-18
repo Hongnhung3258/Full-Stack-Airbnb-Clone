42ad23eb85c3b63f053ae8be6845174ad79d8421
Hồng Nhung <20010877@st.phenikaa-uni.edu.vn>
2024-10-18 17:45:38 +0700

    Update index.ts

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,6 @@
 import { Conversation, Message, User } from "@prisma/client";
+import { create } from "zustand";
+import { ActiveListStore } from "../hooks/useActiveList";
 
 export type FullMessageType = Message & {
     sender: User,
@@ -9,3 +11,9 @@ export type FullConversationType = Conversation & {
     users: User[],
     messages: FullMessageType[],
 };
+export const useActiveList = create<ActiveListStore>((set) => ({
+    members: [],
+    add: (id) => set((state) => ({ members: [...state.members, id] })),
+    remove: (id) => set((state) => ({ members: state.members.filter((memberId) => memberId !== id) })),
+    set: (ids) => set({ members: ids })
+}));
